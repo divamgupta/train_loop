f6183ccab6a2e6925cd947500def5414eef0ba9a
Your Name <you@example.com>
2026-01-14 06:30:13 +0000

    fix extra args

--- train_loop/utils/dynamic_import.py
+++ train_loop/utils/dynamic_import.py
@@ -1,5 +1,6 @@
 import importlib
 from .model_loading import load_model_weights, load_weights_pt
+import copy
 
 def build_class(config, extra_args={}):
     """
@@ -11,7 +12,11 @@ def build_class(config, extra_args={}):
     cls = getattr(module, class_name)
     args = config.get('args', {})
     ckpt_path = config.get('ckpt_path', None)
-    m = cls(**args, **extra_args)
+    if len(extra_args) > 0:
+        args = copy.deepcopy(args)
+        for k in extra_args:
+            args[k] = extra_args[k]
+    m = cls(**args)
     if ckpt_path is not None:
         if "ckpt_ignore_prefix" in config and config['ckpt_ignore_prefix'] is not None:
             ignore_prefix = config['ckpt_ignore_prefix']
